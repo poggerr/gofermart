dcc74b74499d92894e3e0b6a267d3603d77e1bf5
poggerr <etor0pov@yandex.ru>
2023-07-18 21:11:25 +0300

    some refactor

--- internal/models/models.go
+++ internal/models/models.go
@@ -26,7 +26,7 @@ type UserOrder struct {
 type Orders []UserOrder
 
 type Withdraw struct {
-	OrderNumber string    `db:"order_number" json:"order"`
+	OrderNumber int       `db:"order_number" json:"order"`
 	Sum         float32   `db:"sum" json:"sum"`
 	ProcessedAt time.Time `db:"processed_at" json:"processed_at"`
 }
