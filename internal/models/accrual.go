80b2b8f58582d796dc939216ebdb7b44e71bf1e6
poggerr <etor0pov@yandex.ru>
2023-08-01 00:15:13 +0300

    some refactor v3

--- internal/models/accrual.go
+++ internal/models/accrual.go
@@ -3,5 +3,5 @@ package models
 type Accrual struct {
 	Order   string  `json:"order"`
 	Status  string  `json:"status"`
-	Accrual float32 `json:"accrual_service"`
+	Accrual float32 `json:"accrual"`
 }
